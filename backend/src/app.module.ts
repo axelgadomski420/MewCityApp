19f8cdf26378f6904b166cfe954a17806ac80fef
axelgadomski420 <axelgadomski@gmail.com>
2025-09-09 12:07:05 +0200

    Create app.module.ts

---
+++ backend/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'data/db.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UserModule,
+    AuthModule,
+  ],
+})
+export class AppModule {}
