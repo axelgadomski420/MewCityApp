b13f509bc483427c3a564f7f9091e4305cb8a438
axelgadomski420 <axelgadomski@gmail.com>
2025-09-09 12:25:49 +0200

    Create auth.service.ts

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '../user/user.entity';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly jwtService: JwtService) {}
+
+  login(user: User) {
+    const payload = { email: user.email, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
