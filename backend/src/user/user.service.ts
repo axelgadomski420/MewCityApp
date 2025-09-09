f08cfad488db1ebacbd4b5f5cc3f81e5c2527647
axelgadomski420 <axelgadomski@gmail.com>
2025-09-09 12:02:43 +0200

    Create user.service.ts

---
+++ backend/src/user/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async register(email: string, password: string) {
+    const user = this.userRepository.create({ email, password });
+    return this.userRepository.save(user);
+  }
+
+  async findByEmail(email: string): Promise<User | undefined> {
+    return this.userRepository.findOne({ where: { email } });
+  }
+}
